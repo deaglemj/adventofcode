4b711d72425d5946033eab2e8e59d334b92fd3a0
Mikkel Jensen <jensen.mikkel@gmail.com>
2022-12-01 18:54:30 +0100

    init

---
+++ src/test/java/dk/mikkel/adventofcode/AdventofcodeApplicationTests.java
@@ -0,0 +1,15 @@
+package dk.mikkel.adventofcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventofcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue( true);
+	}
+
+}
