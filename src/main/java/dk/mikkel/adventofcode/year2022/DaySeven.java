45c02049a0a6f5490f9dcacadd60f2f7f02e6e5c
Mikkel Jensen <jensen.mikkel@gmail.com>
2022-12-07 21:58:19 +0100

    day seven

---
+++ src/main/java/dk/mikkel/adventofcode/year2022/DaySeven.java
@@ -0,0 +1,88 @@
+package dk.mikkel.adventofcode.year2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DaySeven {
+
+    private static final int LIMIT = 100000;
+    private static final int FILESYSTEMSIZE = 70000000;
+    private static final int MINIMUMFREESIZE = 30000000;
+    private static Logger logger = LogManager.getLogger(DaySeven.class);
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/year2022/day_seven.txt"));
+
+        String[] meta = { "dir", "/" };
+        List<Integer> sizes = new ArrayList<>();
+        File currentDirectory = new File(meta);
+        int lineCount = lines.size();
+
+        for (int i = 1; i < lineCount; i++) {
+            String line = lines.get(i);
+            String[] split = line.split(" ");
+            if ("$".equals(split[0])) {
+                if ("cd".equals(split[1])) {
+                    if (currentDirectory.files.containsKey(split[2])) {
+                        currentDirectory.files.get(split[2]).parent = currentDirectory;
+                        currentDirectory = currentDirectory.files.get(split[2]);
+                    } else {
+                        currentDirectory.size = currentDirectory.getSize();
+                        sizes.add(currentDirectory.size);
+                        currentDirectory = currentDirectory.parent;
+                    }
+                }
+            } else {
+                File file = new File(split);
+                currentDirectory.files.put(file.name, file);
+            }
+        }
+        while (currentDirectory.parent != null) {
+            currentDirectory.size = currentDirectory.getSize();
+            sizes.add(currentDirectory.size);
+            currentDirectory = currentDirectory.parent;
+
+        }
+        currentDirectory.size = currentDirectory.getSize();
+        sizes.add(currentDirectory.size);
+
+        logger.info(sizes.stream().filter(i -> i < LIMIT).reduce(0, Integer::sum));
+
+        Collections.reverse(sizes);
+        int freeSpace = FILESYSTEMSIZE - currentDirectory.size;
+        logger.info(sizes.stream().filter(i -> (freeSpace + i) > MINIMUMFREESIZE).sorted().limit(1).reduce(0, Integer::sum));
+    }
+
+    private static class File {
+
+        String name;
+        int size = 0;
+        Map<String, File> files = new HashMap<>();
+        File parent = null;
+
+        public File(String[] meta) {
+            this.name = meta[1];
+            if (!"dir".equals(meta[0])) {
+                size = Integer.parseInt(meta[0]);
+            }
+        }
+
+        public int getSize() {
+            return files.entrySet().stream().map(es -> es.getValue().size).reduce(0, Integer::sum);
+        }
+    }
+
+}
