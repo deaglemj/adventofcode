f8d5ee45b625d843605eba3950d2d3504449cea9
Mikkel Jensen <jensen.mikkel@gmail.com>
2022-12-13 21:29:02 +0100

    day 10 part two

--- src/main/java/dk/mikkel/adventofcode/year2022/Day10.java
+++ src/main/java/dk/mikkel/adventofcode/year2022/Day10.java
@@ -2,6 +2,7 @@ package dk.mikkel.adventofcode.year2022;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Stack;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,30 +27,45 @@ public class Day10 {
 
         Integer cycle = 0;
         int signalStrength = 1;
-        int sum = 0;
-        List<Integer> controlPoints  = List.of(20, 60, 100, 140, 180, 220);
-
+        int sumPartOne = 0;
+        Stack<String> partTwo = new Stack<>();
+        partTwo.add("");
         for (String line : lines) {
             if(NOOP.equals(line)){
-                if(controlPoints.contains(++cycle)){
-                    sum += cycle * signalStrength;
-                }
+                sumPartOne = cycleControl(++cycle, signalStrength, sumPartOne, partTwo);
             }else{
                 String[] split = line.split(" ");
-                if(controlPoints.contains(++cycle)){
-                    sum += cycle * signalStrength;
-                }
-                if(controlPoints.contains(++cycle)){
-                    sum += cycle * signalStrength;
-                }
+                sumPartOne = cycleControl(++cycle, signalStrength, sumPartOne, partTwo);
+                sumPartOne = cycleControl(++cycle, signalStrength, sumPartOne, partTwo);
                 signalStrength += Integer.parseInt(split[1]);
             }
-            logger.info(cycle);
-            logger.info(signalStrength);
-
-            System.out.println("");
         }
-        logger.info(sum);
+        logger.info(sumPartOne);
+        partTwo.forEach(logger::info);
+    }
+
+
+
+    private static int cycleControl(Integer cycle, int signalStrength, int sumPartOne, Stack<String> partTwo) {
+        if(List.of(20, 60, 100, 140, 180, 220).contains(cycle)){
+            sumPartOne += cycle * signalStrength;
+        }
+        String pop = partTwo.pop();
+        pop += extracted(cycle%40, signalStrength) ? "#" : ".";
+        partTwo.add(pop);
+        if(cycle%40==0){
+            partTwo.add("");
+        }
+        return sumPartOne;
+    }
+
+
+
+    private static boolean extracted(Integer cycle, int signalStrength) {
+        boolean check_one = cycle -1 == signalStrength + 1;
+        boolean check_two = cycle   == signalStrength + 1;
+        boolean check_three = cycle + 1 == signalStrength + 1;
+        return check_one || check_two  || check_three ;
     }
 
 
