cca1a57e4e733020b3a77af9d31e6b3899cd7959
Mikkel Jensen <mj@lessor.dk>
2024-12-03 12:11:12 +0100

    Complete day3 2024

--- src/main/java/dk/mikkel/adventofcode/year2024/Year2024Day03.java
+++ src/main/java/dk/mikkel/adventofcode/year2024/Year2024Day03.java
@@ -1,10 +1,17 @@
 package dk.mikkel.adventofcode.year2024;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import dk.mikkel.adventofcode.util.FileReader;
 
 public class Year2024Day03 {
+
+    private static Logger logger = LogManager.getLogger(Year2024Day03.class);
 
     public static void main(String[] args) {
         new Year2024Day03().runner();
@@ -18,11 +25,54 @@ public class Year2024Day03 {
     }
 
     public void puzzleOne(List<String> instructions) {
+        logger.info("Puzzle 1");
 
-        
+        Status status = new Status();
+        final String regex = "mul\\((\\d+),(\\d+)\\)";
+        instructions.stream().forEach(t -> {
+            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+            final Matcher matcher = pattern.matcher(t);
+
+            while (matcher.find()) {
+                status.sum += Long.valueOf(matcher.group(1)) * Long.valueOf(matcher.group(2));
+            }
+
+        });
+
+        logger.info(status.sum);
+
     }
 
     public void puzzleTwo(List<String> instructions) {
+        logger.info("Puzzle 2");
+
+        Status status = new Status();
+
+        final String regex = "mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)";
+        instructions.stream().forEach(t -> {
+            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+            final Matcher matcher = pattern.matcher(t);
+
+            while (matcher.find()) {
+
+                if (matcher.group().equals("do()")) {
+                    status.enable = true;
+                } else if (matcher.group().equals("don't()")) {
+                    status.enable = false;
+                } else {
+                    if (status.enable) {
+                        status.sum += Long.valueOf(matcher.group(1)) * Long.valueOf(matcher.group(2));
+                    }
+                }
+            }
+        });
+
+        logger.info(status.sum);
+    }
+
+    private class Status {
+        boolean enable = true;
+        long sum = 0;
     }
 
 }
