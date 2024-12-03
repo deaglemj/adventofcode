44d58cc244df7837e1dc29aa47c8e605eddcc1ae
Mikkel Jensen <jensen.mikkel@gmail.com>
2024-12-03 07:06:16 +0100

    init day 3 2024

---
+++ src/main/java/dk/mikkel/adventofcode/year2024/Year2024Day3.java
@@ -0,0 +1,28 @@
+package dk.mikkel.adventofcode.year2024;
+
+import java.util.List;
+
+import dk.mikkel.adventofcode.util.FileReader;
+
+public class Year2024Day3 {
+
+    public static void main(String[] args) {
+        new Year2024Day3().runner();
+    }
+
+    public void runner() {
+
+        List<String> instructions = FileReader.readFileToList("day_8.txt");
+        puzzleOne(instructions);
+        puzzleTwo(instructions);
+    }
+
+    public void puzzleOne(List<String> instructions) {
+
+        
+    }
+
+    public void puzzleTwo(List<String> instructions) {
+    }
+
+}
