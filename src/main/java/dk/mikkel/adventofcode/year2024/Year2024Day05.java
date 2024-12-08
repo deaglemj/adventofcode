4b404335d21f95594b7b6f19bcbd3b5bf76a9c19
Mikkel Jensen <jensen.mikkel@gmail.com>
2024-12-08 09:22:32 +0100

    Complete day 5 2024

--- src/main/java/dk/mikkel/adventofcode/year2024/Year2024Day05.java
+++ src/main/java/dk/mikkel/adventofcode/year2024/Year2024Day05.java
@@ -19,34 +19,104 @@ public class Year2024Day05 {
 
     public void runner() {
 
-        List<String> input = FileReader.readFileToList(2024, 5, true);
+        List<String> input = FileReader.readFileToList(2024, 5);
 
         List<Pair<String, String>> rules = new ArrayList<>();
-        List<String> updates = new ArrayList<>();
+        List<String[]> updates = new ArrayList<>();
 
         for (String line : input) {
             if (line.contains("|")){
                 String[] parts = line.split("\\|");
                 rules.add(new Pair<>(parts[0], parts[1]));
             }else if (line.contains(",")){
-                updates.add(line);
+                updates.add(line.split(","));
             }
         }
 
-        puzzleOne(rules, updates);
-        puzzleTwo(rules, updates);
+        List<String[]> invalidUpdates = puzzleOne(rules, updates);
+        puzzleTwo(rules, invalidUpdates);
     }
 
-    public void puzzleOne(List<Pair<String, String>> rules, List<String> updates) {
+    public List<String[]> puzzleOne(List<Pair<String, String>> rules, List<String[]> updates) {
         logger.info("Puzzle 1");
 
+        List<String[]> invalidUpdates = new ArrayList<>();
+
+        int sum = 0;
+        for (String[] update : updates) {
+            boolean isRuleBroken = false;
+            for (Pair<String, String> rule : rules) {
+                int a = -1;
+                int b = -1;
+                for (int i = 0; i < update.length; i++) {
+                    if (update[i].equals(rule.a)) {
+                        a = i;
+                    }
+                    if (update[i].equals(rule.b)) {
+                        b = i;
+                    }
+                }
+                if (a != -1 && b != -1 && a > b) {
+                    isRuleBroken |= true;
+                    invalidUpdates.add(update);
+                    break;
+                }
+
+                
+            }
+            if (!isRuleBroken) {
+                sum += Integer.valueOf(update[update.length / 2]);
+            }
+        }
+
+        logger.info("Sum: " + sum);
+        return invalidUpdates;
+
     }
 
-    public void puzzleTwo(List<Pair<String, String>> rules, List<String> updates) {
+    public void puzzleTwo(List<Pair<String, String>> rules, List<String[]> updates) {
         logger.info("Puzzle 2");
 
-        
+        logger.info("Updates: " + updates.size());
+
+        int i = 0;
+        int sum = 0;
+        while(i < updates.size()){
+            String[] update = updates.get(i);
+            boolean isRuleBroken = false;
+            
+            for (Pair<String, String> rule : rules) {
+                int a = -1;
+                int b = -1;
+                for (int j = 0; j < update.length; j++) {
+                    if (update[j].equals(rule.a)) {
+                        a = j;
+                    }
+                    if (update[j].equals(rule.b)) {
+                        b = j;
+                    }
+                }
+                // A and B swap places
+                if (a != -1 && b != -1 && a > b) {
+                    String temp = update[a];
+                    update[a] = update[b];
+                    update[b] = temp;
+                    isRuleBroken = true;
+                }
+            }
+
+            if (!isRuleBroken) {
+                i++;
+                sum += Integer.valueOf(update[update.length / 2]);
+            }
+
+
+
+        }
+        logger.info(sum);
     }
+
+
 
 
 
